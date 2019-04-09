1472f4735c6ccfe812d525efa59f25de4b427468
Liliia111 <34775878+Liliia111@users.noreply.github.com>
2019-04-09 13:31:35 +0300

    Add files via upload

---
+++ web_dev/project22/createcon.php
@@ -0,0 +1,78 @@
+
+<html lang="en">
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <title>Project 2</title>
+  <style>
+
+      body {
+        background-color: #bfbfbf;
+        font-family: 'Lato', sans-serif;
+        color: #50514F;
+
+      }
+input[type=text], select {
+width: 100%;
+font-family: inherit;
+padding: 12px 20px;
+margin: 8px 0;
+display: inline-block;
+border: none;
+border-radius: 4px;
+box-sizing: border-box;
+
+}
+
+input[type=submit] {
+width: 100%;
+background-color: #d45d7d;
+color: white;
+padding: 14px 20px;
+margin: 8px 0;
+border: none;
+border-radius: 4px;
+cursor: pointer;
+font-family: inherit;
+}
+
+input[type=submit]:hover {
+background-color:#d45d7d;
+}
+
+div {
+  width: 450px;
+  margin: 20% auto;
+}
+</style>
+<body>
+<iframe src="menu.html" width="100%" style="height: 300px; border:0; "></iframe>
+<h3 style = "font-weight: bold; text-align: center">CONNECT</h3>
+
+<div>
+<form action="">
+  <label for="fname" style = "font-weight: bold;">username</label>
+  <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+  <label for="lname" style = "font-weight: bold">servername</label>
+  <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+  <label for="lname" style = "font-weight: bold">password</label>
+  <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+
+
+  <input type="submit" value="Submit">
+</form>
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    </body>
